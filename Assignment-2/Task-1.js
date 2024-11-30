38795fa42a78350cc4839ab0dc786d8c0c611a6e
Hanan Sultan <hanan.bhatti.39@gmail.com>
2024-11-30 17:00:22 +0500

    Second Assignment started

---
+++ Assignment-2/Task-1.js
@@ -0,0 +1,15 @@
+
+let firstName = 'Tommy';
+let middleName = 'Lee';
+let lastName = 'Howell';
+
+
+console.log(firstName, middleName, lastName);
+
+
+firstName = "Tommy";
+middleName = "Lee";
+lastName = "Howell";
+
+
+console.log(firstName, middleName, lastName);
